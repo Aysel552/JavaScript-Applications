77e4f04e0cd3bac14a0f08698a12d3a89635c582
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-02-13 21:41:01 +0400

    used replace method

---
+++ Variables/script.js
@@ -0,0 +1,84 @@
+// Example1
+
+// let books = 7;
+// alert(books);
+
+
+// Example2
+
+// let country = "Italy";
+// alert(country);
+
+// How o hide a code from a browser:
+/*let country = "France";
+alert(country);
+*/
+
+// How to check a data type?
+// let age = 30;
+// console.log(typeof age);
+
+// let tomatoes = 4;
+// let cucumber = 3;
+// let milk = 6;
+// let cheese = 8;
+// let wine = 25;
+
+// let products = tomatoes + cucumber + milk + cheese + wine;
+// console.log(products);
+
+// let salary = 4000;
+// let rent = 2000;
+// let groceries = 1000;
+// let wiFi = 100;
+
+// let money = salary - (rent- groceries-wiFi)
+// console.log(money);
+
+// let salary = 100000;
+// let federalTax = 12741;
+// let provincialTax = 15557;
+// let otherTaxes = 3631;
+
+// let money = salary - federalTax - provincialTax - otherTaxes;
+// console.log(money);
+
+// let greeting = "Welcome ";
+// let user = "Steve! ";
+// let message = "I hope you like it here."
+
+// let userMessage = greeting + user + message;
+// alert(userMessage)
+
+// let user = 'Steve';
+// let message = `Hello ${user}`;
+// console.log(message);
+
+// let oranges = 10;
+// let apples = 5;
+// let tomatoes = 3;
+// alert(`You bought ${oranges} oranges, ${apples} apples and ${tomatoes} tomatoes`)
+
+
+// let box = 'toys';
+// let boxTransformed = box.toUpperCase();
+// let boxSize = box.toLowerCase();
+
+// console.log(boxTransformed);
+// console.log(boxSize);
+
+// Replace Method
+let text = "Hello";
+let user = "Steve";
+let question = "how are you?"
+let result = text.concat(" ", user, " ", question);
+console.log(result);
+
+let phrase = "I love CSS";
+let output = phrase.replace('CSS', 'JavaScript');
+let outputModified = output.replace('love', 'hate');
+console.log(outputModified);
+
+
+
+
